e015accdd67762e3b53add1f7f395516cfd6d99b
hexu <hexu_1985@sina.com>
2021-06-15 20:25:44 +0800

    ch4编译通过

--- src/ch4/prg4_2.cpp
+++ src/ch4/prg4_2.cpp
@@ -24,65 +24,65 @@ void join (vector<T> &vA, const vector<T>& vB);
 
 int main()
 {
-	// declare 3 integer vectors
-   vector<int> vSmall, vMedium, vLarge;
-	// use a random number generator
-	randomNumber rnd;
+    // declare 3 integer vectors
+    vector<int> vSmall, vMedium, vLarge;
+    // use a random number generator
+    randomNumber rnd;
 
-   int i, value;
+    int i, value;
 
-	for (i = 0; i < 12; i++)
-	{
-		value = rnd.random(900) + 100;
+    for (i = 0; i < 12; i++)
+    {
+        value = rnd.random(900) + 100;
 
-		if (value < 400)
-			vSmall.push_back(value);
-		else if (value < 700 )
-			vMedium.push_back(value);
-		else
-			vLarge.push_back(value);
-	}
+        if (value < 400)
+            vSmall.push_back(value);
+        else if (value < 700 )
+            vMedium.push_back(value);
+        else
+            vLarge.push_back(value);
+    }
 
-	// sort the vector of integers in the range
-	// 100 <= n < 400 and output
-	insertionSort(vSmall);
-	cout << "Small:   ";
-	writeVector(vSmall);
+    // sort the vector of integers in the range
+    // 100 <= n < 400 and output
+    insertionSort(vSmall);
+    cout << "Small:   ";
+    writeVector(vSmall);
 
-	// sort the vector of integers in the range
-	// 400 <= n < 700 and output
-	insertionSort(vMedium);
-	cout << "Medium:  ";
-	writeVector(vMedium);
+    // sort the vector of integers in the range
+    // 400 <= n < 700 and output
+    insertionSort(vMedium);
+    cout << "Medium:  ";
+    writeVector(vMedium);
 
-	// sort the vector of integers in the range
-	// 700 <= n < 1000 and output
-	insertionSort(vLarge);
-	cout << "Large:   ";
-	writeVector(vLarge);
+    // sort the vector of integers in the range
+    // 700 <= n < 1000 and output
+    insertionSort(vLarge);
+    cout << "Large:   ";
+    writeVector(vLarge);
 
-	// join vMedium onto the end of vSmall
-	join(vSmall, vMedium);
-	// join vLarge onto the end of the modified vSmall
-	join(vSmall, vLarge);
+    // join vMedium onto the end of vSmall
+    join(vSmall, vMedium);
+    // join vLarge onto the end of the modified vSmall
+    join(vSmall, vLarge);
 
-	// output the new vector
-	cout << "Sorted:  ";
-	writeVector(vSmall);
+    // output the new vector
+    cout << "Sorted:  ";
+    writeVector(vSmall);
 
-	return 0;
+    return 0;
 }
 
-template <typename T>
+    template <typename T>
 void join (vector<T> &vA, const vector<T>& vB)
 {
-	// capture the size of vB in sizeB
-	int i, sizeB = vB.size();
+    // capture the size of vB in sizeB
+    int i, sizeB = vB.size();
 
-	// use index i to access the elements of vB and push_back()
-	// to add the elements to the rear of vA
-   for (i = 0; i < sizeB; i++)
-      vA.push_back(vB[i]);
+    // use index i to access the elements of vB and push_back()
+    // to add the elements to the rear of vA
+    for (i = 0; i < sizeB; i++)
+        vA.push_back(vB[i]);
 }
 
 /*
